d519b5e4ab6714c64c1c8b7c853a0d09809f9777
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2025-06-26 13:20:01 -0400

    :sparkles: feat: implement color scheme support and update global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,21 @@
+import { Button } from "@/components/ui/Button";
 import { Text } from "@/components/ui/Text";
+
 import { ThemeToggle } from "@/hooks/useColorScheme";
 import "@/styles/global.css";
+import { useColorScheme } from "nativewind";
 import { View } from "react-native";
 
 
 export default function Index() {
+    const { colorScheme } = useColorScheme();
     return (
-        <View className="pt-8 flex-1  min-h-screen items-center justify-center">
+        <View className={`pt-8 flex-1  min-h-screen items-center justify-center ${colorScheme === "dark" && "dark"}`}>
             <Text>Ola mundo</Text>
             <ThemeToggle />
+            <Button variant={"ghost"}>
+
+            </Button>
         </View>
     )
 }
